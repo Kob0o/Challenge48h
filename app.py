a07831925f6564ad877330638b0f5fa7af9a3968
Ass4sin <tudorandrei2010@yahoo.com>
2025-01-30 11:38:13 +0100

    test

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Test"
+    return render_template("signup.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
